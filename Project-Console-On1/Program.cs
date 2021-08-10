926fe62c01d67c62cd97ee77ab55d2dd4fa9d9a2
m.jawad <m.jawad@smartech-lb.com>
2021-08-10 11:07:32 +0300

    Add project files.

---
+++ Project-Console-On1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Console_On1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
